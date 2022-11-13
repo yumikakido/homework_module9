a7911f93c326693fd8187ffcd69091ce9ad72b2b
Yumika <yumika0904@live.jp>
2022-11-13 10:39:04 +0800

    completed coding

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicense(license) {
+  switch (license) {
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    case 'GNU GPL v3':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 
-// TODO: Create a function to generate markdown for README
+    case 'BSD 3-Clause License':
+      return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+
+    default: return ''
+ }
+}
+
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `${renderLicense(data.license)}
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This code base is covered under the ${data.license} license. For more information click: ${renderLicense(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+[GitHub](https://github.com/${data.githubUsername})
+Contact me using my email (${data.email}) if you have questions.
 
 `;
 }
